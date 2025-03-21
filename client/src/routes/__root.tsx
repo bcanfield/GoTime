b85dbf2bba1631148531c4d41ed42caf1297a5cb
CANFIELD.BRANDIN.W.1078680373.E <bcanfield@miletwo.us>
2025-03-20 21:01:18 -0400

    Checkin

---
+++ client/src/routes/__root.tsx
@@ -0,0 +1,45 @@
+import { Link, Outlet, createRootRoute } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import { Drawer, DrawerButton } from "../components/daisy/drawer";
+
+export const Route = createRootRoute({
+  component: RootComponent,
+});
+
+function RootComponent() {
+  return (
+    <>
+      <div className="flex h-screen bg-base-100">
+        <div className="mx-auto max-w-6xl border w-full">
+          <nav className="h-12 bg-green-500 flex items-center">
+            <DrawerButton />
+            <Link
+              to="/"
+              activeProps={{
+                className: "font-bold",
+              }}
+              activeOptions={{ exact: true }}
+            >
+              Home
+            </Link>
+            <Link
+              to="/about"
+              activeProps={{
+                className: "font-bold",
+              }}
+              activeOptions={{ exact: true }}
+            >
+              About
+            </Link>
+          </nav>
+
+          <Drawer>
+            <Outlet />
+          </Drawer>
+        </div>
+      </div>
+      <hr />
+      <TanStackRouterDevtools position="bottom-right" />
+    </>
+  );
+}
